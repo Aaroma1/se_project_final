cd40493cef0bdff7899e292c4c6c2f20c902ab6d
Aaron Romano <aaronjromano1@gmail.com>
2025-10-08 14:02:19 -0700

    fix path for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/se_project_final/" : "/",
-  server: {
-    port: 3001,
-  },
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    server: {
+      port: 3001,
+    },
+  };
+  
+  if (command === 'build') {
+    config.base = '/se_project_final/';
+  }
+  
+  return config;
 });
