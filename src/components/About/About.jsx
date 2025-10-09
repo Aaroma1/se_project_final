2cd885f3af443270651b2fc29e8d9e3e5340888a
Aaron Romano <aaronjromano1@gmail.com>
2025-10-08 20:26:02 -0700

    hash router change clean up html

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ function About() {
       <div className="about__avatar-container">
         <img
           className="about__avatar"
-          src="./Aaron-Main-Headshot.jpg"
+          src="/Aaron-Main-Headshot.jpg"
           alt="Author avatar"
         />
       </div>
